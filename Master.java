b3968e4d38a4c074fef4befb15a5445ff79f0dfc
pbmulinzi <pbmulinzi@gmail.com>
2024-02-27 18:48:11 +0300

    Testing my github

--- Master.java
+++ Master.java
@@ -1,5 +1,6 @@
 public class Master{
     public static void main(String[] args){
         System.out.println("Trying to correct some issues here!");
+        System.out.println("Smile Uganda!");
     }
 }
