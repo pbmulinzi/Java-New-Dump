3566e758ab607a6575b2f90f080d4fd1c91684b1
pbmulinzi <pbmulinzi@gmail.com>
2024-02-27 18:23:41 +0300

    Connecting my java code to my github

---
+++ Correct.java
@@ -0,0 +1,7 @@
+public class Correct{
+    public static void main(String[] args){
+        System.out.println("Smile Uganda :)");
+        System.out.println("This is a good track!");
+        System.out.println("Practicing Java");
+    }
+}
